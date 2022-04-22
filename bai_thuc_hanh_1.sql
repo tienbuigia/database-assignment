9042741538ed3d1e9377be2dc24eef98d14f55b5
Tien <62953368+tienbuigia@users.noreply.github.com>
2022-04-22 21:55:14 +0700

    First assignment with 8/12 tasks done.

---
+++ bai_thuc_hanh_1.sql
@@ -0,0 +1,48 @@
+-- #1
+SELECT 
+	Title,FirstName,MiddleName,LastName
+FROM 
+	Person.Person;
+-- #2
+SELECT 
+	ISNULL(Title, '') + ' ' + FirstName + ' ' + LastName AS PersonName
+FROM 
+	Person.Person;
+-- #3
+SELECT 
+	AddressLine1 + ' ' + ISNULL(AddressLine2, '') AS AddressLine, City 
+FROM 
+	Person.Address;
+-- #4
+SELECT DISTINCT City 
+FROM 
+	Person.Address;
+-- #5
+SELECT TOP 10 * FROM Person.Address;
+-- #6
+SELECT AVG(Rate) AS AverageRate FROM HumanResources.EmployeePayHistory;
+-- #7
+SELECT COUNT(*) AS #Employees FROM HumanResources.Employee;
+-- #8
+WITH SalesPerCustomer AS 
+(
+    SELECT 
+        c.CustomerID,
+        NumberOfSales = COUNT(soh.SalesOrderID)
+    FROM 
+        Sales.Customer c 
+    INNER JOIN 
+        Sales.SalesOrderHeader soh ON soh.CustomerID = c.CustomerID
+    GROUP BY    
+        c.CustomerID    
+)
+SELECT 
+    p.FirstName + ' ' + p.LastName AS CustomerName,
+    spc.NumberOfSales
+FROM 
+    SalesPerCustomer spc
+JOIN
+	Person.Person p ON p.BusinessEntityID = spc.CustomerID
+WHERE 
+	spc.NumberOfSales > 10;
+-- #9
