89a2ef4f1375bef29a6b66726cf61cd1a02a3e0b
tien <tien.bg187345@sis.hust.edu.vn>
2022-06-14 11:26:51 +0700

    done 1-3

---
+++ week34/20187345_BuiGiaTien_bai4.sql
@@ -0,0 +1,17 @@
+﻿-- #1:
+PRINT N'Xin chào';
+GO
+-- #2:
+DECLARE @ten varchar(50)
+SET @ten = N'Tiến';
+PRINT N'Xin chào ' + @ten;
+GO
+-- #3:
+DECLARE @s1 int = 520,
+		@s2 int = 250,
+		@tg int;
+SET @tg = @s1 + @s2;
+PRINT N'tổng là: ' + CAST(@tg AS NVARCHAR(20));
+GO
+-- #4:
+--> tính tổng
