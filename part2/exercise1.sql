49c7b4904a0103e1c97ea19ff12dccebf05b1159
tien <tien.bg187345@sis.hust.edu.vn>
2022-07-17 18:47:09 +0700

    part2-ex1 init

---
+++ part2/exercise1.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE TestDB;
+GO
+USE TestDB;
+GO
+CREATE SCHEMA Test;
+GO
+CREATE TABLE Test.Accounts (
+	AccountNumber INT PRIMARY KEY
+);
+CREATE TABLE Test.AccountTransactions (
+	TransactionID INT IDENTITY PRIMARY KEY
+	,AccountNumber INT NOT NULL REFERENCES Test.Accounts
+	,CreatedDateTime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+	,Amount DECIMAL(19, 5) NOT NULL
+);
+GO
+CREATE PROC Test.spAccountReset
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DELETE Test.AccountTransactions;
+	DELETE Test.Accounts;
+	INSERT Test.Accounts (AccountNumber) VALUES (1001);
+	INSERT Test.AccountTransactions (AccountNumber, Amount)
+	VALUES (1001, 100);
+	INSERT Test.AccountTransactions (AccountNumber, Amount)
+	VALUES (1001, 500);
+	INSERT Test.AccountTransactions (AccountNumber, Amount)
+	VALUES (1001, 1400);
+	SELECT AccountNumber, SUM(Amount) AS Balance
+	FROM Test.AccountTransactions
+	GROUP BY AccountNumber;
+END
