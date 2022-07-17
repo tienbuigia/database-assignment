547752be0a4bf53f4b3df2442ce4986345d6c9ce
tien <tien.bg187345@sis.hust.edu.vn>
2022-07-17 22:40:44 +0700

    answer last question: what concurrency problem?

--- part2/ex1/connection1.sql
+++ part2/ex1/connection1.sql
@@ -9,5 +9,5 @@ FROM Test.AccountTransactions
 WHERE AccountNumber = 1001;
 GO
 EXEC Test.spAccountWithdraw @AccountNumber = 1001, @AmountToWithdraw = 2000;
-/* What kind of concurrency problem occurred? */
-
+/* What kind of concurrency problem occurred? 
+	it's "phantom reads" */
