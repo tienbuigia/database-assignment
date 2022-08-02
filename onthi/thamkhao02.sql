b011d83a2b34c752ccafc51d79df6c2ef8f78365
art <tien.bg187345@sis.hust.edu.vn>
2022-08-02 22:22:56 +0700

    1d

--- onthi/thamkhao02.sql
+++ onthi/thamkhao02.sql
@@ -22,3 +22,15 @@ FROM NhomMatHang nmh
   JOIN Mathang mhh ON mhh.NhomHang = nmh.MaNhomMH
 WHERE TenNhomMH = N'Hàng gia dụng'
 GROUP BY MaNhomMH;
+
+-- 1d:
+SELECT MaKH, MaMH, count(SoLuong) as SoLuongBan INTO temp
+FROM MuaHang mh
+WHERE NgayMua BETWEEN '2015-01-01' AND '2015-10-25'
+GROUP BY MaMH, MaKH;
+
+SELECT TOP 1 mhh.MaKH, SUM(SoLuongBan*DonGia) as doanhthu
+FROM Mathang mhh
+  JOIN temp ON temp.MaMH = mhh.MaMH;
+GROUP BY mhh.MaKH
+ORDER BY SUM(SoLuongBan*DonGia) DESC;
